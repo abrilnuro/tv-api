229215c8347b8c3a0d23ce84073dc6893b388458
abril <13170789@itculiacan.edu.mx>
2019-07-16 23:43:07 -0600

    adding project

---
+++ src/main/java/com/abril/tvapi/TvApiApplication.java
@@ -0,0 +1,13 @@
+package com.abril.tvapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TvApiApplication.class, args);
+	}
+
+}
